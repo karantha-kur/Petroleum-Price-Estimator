251e8f0ebf913a2a3a435c8b4c923e73a9b7bf1e
Karan Thakur <80699988+karantha-kur@users.noreply.github.com>
2023-09-21 19:16:36 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages,setup
+from typing import List
+
+hypen_e_dot='-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+        
+        if hypen_e_dot in requirements:
+            requirements.remove(hypen_e_dot)
+    
+    return requirements
+
+
+setup(
+name='Petroleum Price Forcasting',
+version='1',
+author='Karan Thakur',
+author_email='kt.26karanthakur@gmail.com',
+packages=find_packages(),
+)
